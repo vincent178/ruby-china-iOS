f559dd3e522e76cbe342ac0eed29f74ac4dda583
vincent178 <vincent.007.cn@gmail.com>
2013-06-21 08:07:11 +0800

    Initial Commit

---
+++ RubyChina-for-IOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RubyChina-for-IOS
+//
+//  Created by Vincent on 13-6-21.
+//  Copyright (c) 2013年 Vincent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
