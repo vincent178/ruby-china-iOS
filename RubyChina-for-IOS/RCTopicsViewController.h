2d989a81ff17e90a9c83cb05ce4fed6b78b1a5b1
vincent178 <vincent.007.cn@gmail.com>
2013-06-21 23:48:56 +0800

    test for nsurlrequest

---
+++ RubyChina-for-IOS/RCTopicsViewController.h
@@ -0,0 +1,13 @@
+//
+//  RCTopicsViewController.h
+//  RubyChina-for-IOS
+//
+//  Created by Vincent on 13-6-21.
+//  Copyright (c) 2013年 Vincent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCTopicsViewController : UIViewController
+
+@end
---
+++ RubyChina-for-IOS/RCTopicsViewController.m
@@ -0,0 +1,37 @@
+//
+//  RCTopicsViewController.m
+//  RubyChina-for-IOS
+//
+//  Created by Vincent on 13-6-21.
+//  Copyright (c) 2013年 Vincent. All rights reserved.
+//
+
+#import "RCTopicsViewController.h"
+
+@interface RCTopicsViewController ()
+@end
+
+@implementation RCTopicsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
