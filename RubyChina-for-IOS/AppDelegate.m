3d315257d33e26049c157fe0ce56e817a7bda6f4
vincent178 <vincent.007.cn@gmail.com>
2013-07-26 13:03:08 +0800

    clean code

--- RubyChina-for-IOS/AppDelegate.m
+++ RubyChina-for-IOS/AppDelegate.m
@@ -12,60 +12,27 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    NSString *baseURL = @"http://pixolity.com/get.php";
-    NSString *urlAsString = [baseURL stringByAppendingString:@"?param1=First&param2=Second"];
-    NSLog(@"the query string is = %@", urlAsString);
-    
-    NSURL *url = [NSURL URLWithString:urlAsString];
-    
-    NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:url];
-    [urlRequest setTimeoutInterval:30.0f];
-    [urlRequest setHTTPMethod:@"POST"];
-    
-    NSString *body = @"bodyParam1=BodyValue1&bodyParam2=BodyValue2";
-    [urlRequest setHTTPBody:[body dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
-    
-    [NSURLConnection sendAsynchronousRequest:urlRequest queue:queue
-                           completionHandler:^(NSURLResponse *response, NSData *data, NSError *error){
-                               if (data && !error) {
-                                   NSString *html = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-                                   NSLog(@"HTML = %@", html);
-                               } else if (!data && !error) {
-                                   NSLog(@"No data translated...");
-                               } else if (error) {
-                                   NSLog(@"Error happened.");
-                               }
-                           }];
     return YES;
 }
 							
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
