3146d76397312e36656f98fc189311022d13edba
vincent178 <vincent.007.cn@gmail.com>
2013-07-28 16:23:49 +0800

    doing topics controller

---
+++ RubyChina-for-IOS/TopicController.h
@@ -0,0 +1,13 @@
+//
+//  TopicController.h
+//  RubyChina-for-IOS
+//
+//  Created by Vincent on 13-7-28.
+//  Copyright (c) 2013年 Vincent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopicController : UITableViewController
+
+@end
