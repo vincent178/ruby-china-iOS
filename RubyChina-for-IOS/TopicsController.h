f71bacf1239a3402b92be7f72d9f8bc2c3bf69d4
vincent178 <vincent.007.cn@gmail.com>
2013-08-05 21:30:16 +0800

    table view controller

---
+++ RubyChina-for-IOS/TopicsController.h
@@ -0,0 +1,13 @@
+//
+//  TopicsController.h
+//  RubyChina
+//
+//  Created by Vincent on 13-8-5.
+//  Copyright (c) 2013年 Vincent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopicsController : UITableViewController
+
+@end
