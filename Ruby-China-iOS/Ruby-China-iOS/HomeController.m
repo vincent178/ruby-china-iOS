4eddf289f2c7ee2d5c5244f134d8025379ef9493
vincent178 <vincent.007.cn@gmail.com>
2013-10-10 23:02:01 +0800

    refactor view controller to home controller

--- Ruby-China-iOS/Ruby-China-iOS/HomeController.m
+++ Ruby-China-iOS/Ruby-China-iOS/HomeController.m
@@ -6,13 +6,13 @@
 //  Copyright (c) 2013年 Vincent. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "HomeController.h"
 
-@interface ViewController ()
+@interface HomeController ()
 
 @end
 
-@implementation ViewController
+@implementation HomeController
 
 - (void)viewDidLoad
 {
