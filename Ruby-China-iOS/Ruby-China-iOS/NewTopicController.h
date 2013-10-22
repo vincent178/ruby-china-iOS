ab7397ad2aeb5bba4bf094520013251cf9f27efe
vincent178 <vincent.007.cn@gmail.com>
2013-10-22 22:18:24 +0800

    new topic controller

---
+++ Ruby-China-iOS/Ruby-China-iOS/NewTopicController.h
@@ -0,0 +1,13 @@
+//
+//  NewTopicController.h
+//  Ruby-China-iOS
+//
+//  Created by Liang Huang on 10/22/13.
+//  Copyright (c) 2013 Vincent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewTopicController : UIViewController
+
+@end
---
+++ Ruby-China-iOS/Ruby-China-iOS/NewTopicController.m
@@ -0,0 +1,13 @@
+//
+//  NewTopicController.m
+//  Ruby-China-iOS
+//
+//  Created by Liang Huang on 10/22/13.
+//  Copyright (c) 2013 Vincent. All rights reserved.
+//
+
+#import "NewTopicController.h"
+
+@implementation NewTopicController
+
+@end
