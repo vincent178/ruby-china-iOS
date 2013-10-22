0a34e5560b98f82c7c93b909a00704fa94233bfb
vincent178 <vincent.007.cn@gmail.com>
2013-10-23 07:26:43 +0800

    topic controller to show detail of topic

---
+++ Ruby-China-iOS/Ruby-China-iOS/TopicController.h
@@ -0,0 +1,13 @@
+//
+//  TopicController.h
+//  Ruby-China-iOS
+//
+//  Created by Liang Huang on 10/23/13.
+//  Copyright (c) 2013 Vincent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopicController : UIViewController
+
+@end
---
+++ Ruby-China-iOS/Ruby-China-iOS/TopicController.m
@@ -0,0 +1,13 @@
+//
+//  TopicController.m
+//  Ruby-China-iOS
+//
+//  Created by Liang Huang on 10/23/13.
+//  Copyright (c) 2013 Vincent. All rights reserved.
+//
+
+#import "TopicController.h"
+
+@implementation TopicController
+
+@end
