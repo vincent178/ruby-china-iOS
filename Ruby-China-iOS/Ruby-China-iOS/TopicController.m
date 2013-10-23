9809a35d893a3d0e7933844b16516576e827d936
vincent178 <vincent.007.cn@gmail.com>
2013-10-23 22:25:50 +0800

    add load topic detail

--- Ruby-China-iOS/Ruby-China-iOS/TopicController.m
+++ Ruby-China-iOS/Ruby-China-iOS/TopicController.m
@@ -7,31 +7,51 @@
 //
 
 #import "TopicController.h"
+#import "AFNetworking.h"
 #import "TopicCellWithWebView.h"
 
-@implementation TopicController
+@implementation TopicController {
+    NSDictionary *topicDetail;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self refresh];
 }
+
+- (void)refresh {
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    NSString *url = @"http://ruby-china.org/api/v2/topics/14765.json";
+    [manager GET:url parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        topicDetail = responseObject;
+//        NSLog(@"%@", topicDetail);
+        NSLog(@"%@", [topicDetail objectForKey:@"body"]);
+        
+        [self.tableView reloadData];
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"%@", error);
+    }];
+}
+
+#pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 2;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    if (section == 0) {
-        return 1;
-    } else {
-        return 3;
-    }
+    return ((section == 0) ? 1 : 3);
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString *CellIdentifier = @"TopicCell";
     
+    if (indexPath.section == 0) {
+        TopicCellWithWebView *cell = [[TopicCellWithWebView alloc] init];
+    }
+    static NSString *CellIdentifier = @"TopicCell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     
     if (cell == nil) {
