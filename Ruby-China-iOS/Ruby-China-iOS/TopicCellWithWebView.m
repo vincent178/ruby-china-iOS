d9caf5b0f460eb75716dcc6d1833e40424b1d1cf
vincent178 <vincent.007.cn@gmail.com>
2013-10-23 15:27:13 +0800

    topic title label

--- Ruby-China-iOS/Ruby-China-iOS/TopicCellWithWebView.m
+++ Ruby-China-iOS/Ruby-China-iOS/TopicCellWithWebView.m
@@ -13,6 +13,17 @@
 - (void)setupWithTopicDetail:(NSDictionary *)topicDetail {
     
     // Topic Title Label
+    NSString *topicTitle = [topicDetail objectForKey:@"title"];
+    NSDictionary *attributes = @{NSFontAttributeName: [UIFont systemFontOfSize:16.0f]};
+    CGSize topicTitleSize = [topicTitle sizeWithAttributes:attributes];
+    topicTitleLabel = [[UILabel alloc]
+                         initWithFrame:CGRectMake(15.0f, 15.0f, 230.0f, topicTitleSize.height)];
+    topicTitleLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    topicTitleLabel.numberOfLines = 0;
+    topicTitleLabel.text = topicTitle;
+    topicTitleLabel.font = [UIFont boldSystemFontOfSize:16.0f];
+    [topicTitleLabel sizeToFit];
+    [self addSubview:topicTitleLabel];
     
 }
 
