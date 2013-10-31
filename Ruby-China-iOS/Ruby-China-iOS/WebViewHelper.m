0ebebf7ac20449d2d083e85659343b8730898747
vincent178 <vincent.007.cn@gmail.com>
2013-10-31 19:21:58 +0800

    modify

--- Ruby-China-iOS/Ruby-China-iOS/WebViewHelper.m
+++ Ruby-China-iOS/Ruby-China-iOS/WebViewHelper.m
@@ -15,15 +15,20 @@
 }
 
 + (NSString *)setWebViewWithFont:(int)fontSize Html:(NSString *)html andId:(NSString *)id {
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"default_css" ofType:@"css"];
+    NSData *cssData = [NSData dataWithContentsOfFile:filePath options:NSUTF8StringEncoding error:nil];
+    NSString *css = [[NSString alloc] initWithData:cssData encoding:NSUTF8StringEncoding];
+    
     return [NSString stringWithFormat:@"<!DOCTYPE html>"
             "<html>"
             "<head>"
             "<meta charset='utf-8'>"
             "<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;'>"
             "<style>body {max-width: 290px;}</style>"
+            "<style>%@</style>"
             "</head>"
             "<body>"
-            "<div id=%@ style='font-size: %dpx;'> %@ </div></body></html>", id, fontSize, html];
+            "<div id=%@ style='font-size: %dpx;'> %@ </div></body></html>", css, id, fontSize, html];
 }
 
 @end
