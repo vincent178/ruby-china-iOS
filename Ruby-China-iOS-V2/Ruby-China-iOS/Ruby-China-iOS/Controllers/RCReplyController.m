a71a2109ce7514a828e320da7077350ab7277fa5
vincent178 <vincent.007.cn@gmail.com>
2014-06-17 22:35:47 +0800

    update in view controller

--- Ruby-China-iOS-V2/Ruby-China-iOS/Ruby-China-iOS/Controllers/RCReplyController.m
+++ Ruby-China-iOS-V2/Ruby-China-iOS/Ruby-China-iOS/Controllers/RCReplyController.m
@@ -14,8 +14,7 @@
 
 @implementation RCReplyController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
@@ -23,27 +22,14 @@
     return self;
 }
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
